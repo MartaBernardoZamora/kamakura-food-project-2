a979426d750849a156ff2b213f6f78f962f5fdf8
unknown <martabernardozamora@gmail.com>
2024-12-16 14:47:50 +0100

    Initial upload

---
+++ src/components/menu.js
@@ -0,0 +1,33 @@
+//DEBE imprimir en pantalla la información de filtros.
+import {filters, products} from "../data/data.js"
+
+ function addFilter () {
+    const filtersContainer = document.querySelector(".filters-container")
+    filtersContainer.innerHTML = "";
+    filters.forEach(el => {
+        const buttonFilter = document.createElement("button");
+        buttonFilter.textContent = el;
+        filtersContainer.appendChild(buttonFilter);
+        buttonFilter.classList.add("filter");
+     })
+ }
+addFilter();
+
+function addMenuItems() {
+    const menuContainer = document.querySelector(".products-container")
+    menuContainer.innerHTML = "";
+    products.forEach(el => {
+        const productContainer = document.createElement("div")
+        productContainer.classList.add("product-container")
+        productContainer.innerHTML = `<h3>${el.name}</h3>
+        <p>${el.description}</p>
+        <div class= "price-container">
+        <h5>${el.price}</h5>
+        <button class= "add-button" data-id="${el.id}"> Añadir</button>
+        </div>`
+        menuContainer.appendChild(productContainer);
+      })
+}
+addMenuItems();
+export {addFilter, addMenuItems}
+//DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
