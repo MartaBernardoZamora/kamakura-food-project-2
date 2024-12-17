f4a4084c4258911f1df307e9fa604f5317b98b72
Carlotaml21 <carlotaml21@gmail.com>
2024-12-17 09:52:24 +0100

    add new branch

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,2 +1,27 @@
 //DEBE contener las funcionalidades del carrito de compras.
+function blockNegativeQuantity(quantity){
+    if (quantity < 0) {
+    return 0;
+} 
+return quantity;
+}
 
+let quantity = 1;
+
+/*const buttons = document.querySelectorAll(".quantity-container button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => changeQuantity(button))
+
+    });*/
+
+function changeQuantity(button){ 
+    if (button.textContent === "+"){
+        quantity +=1;
+    } else if (button.textContent === "-") {
+        quantity -=1;
+        quantity = blockNegativeQuantity(quantity);
+    } document.querySelector(".quantity").textContent = quantity;
+} 
+
+export{changeQuantity}
--- src/events.js
+++ src/events.js
@@ -1,2 +1,9 @@
 //Intenta separar los eventos en este archivo.
+import { changeQuantity } from "./components/cart.js";
+const buttons = document.querySelectorAll(".quantity-container button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => changeQuantity(button))
+
+    });
 
